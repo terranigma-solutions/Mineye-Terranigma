fddeca86397c21e10865c9bc058873c30a4e0d5c
Miguel de la Varga <miguel@terranigma-solutions.com>
2025-10-29 16:56:57 +0100

    Add functionality to test error propagation for dips
    Introduced a new `TestErrorPropagationDips` class to evaluate error propagation related to orientation dips in geological models. Added methods for modifying dips, updating models, and performing prior inference with Pyro, ensuring proper handling of dip gradients and validations.

--- tests/tests_inversions/Model 1/test_error_propagation.py
+++ tests/tests_inversions/Model 1/test_error_propagation.py
@@ -100,3 +100,93 @@ class TestErrorPropagationMovePoint:
             update_model_fn=self._update_model_for_plotting,
             gempy_plot=p2d
         )
+
+
+class TestErrorPropagationDips:
+    prior_key = r'dips'
+    
+    @staticmethod
+    def _modify_dips_for_orientations(
+            samples: dict[str, Distribution],
+            geo_model: gp.data.GeoModel,
+    ) -> InterpolationInput:
+        # # TODO: We can make a factory for this type of functions
+
+        from gempy.modules.data_manipulation import interpolation_input_from_structural_frame
+        from gempy.modules.data_manipulation.manipulate_points import compute_adp_from_gradients, convert_orientation_to_pole_vector
+
+        interp_input: InterpolationInput = interpolation_input_from_structural_frame(geo_model)
+        smaples = samples[TestErrorPropagationDips.prior_key]
+        
+        og_gradients = interp_input.orientations.dip_gradients
+        azimuth, dip, polarity = compute_adp_from_gradients(
+            G_x=og_gradients[:, 0],
+            G_y=og_gradients[:, 1],
+            G_z=og_gradients[:, 2],
+        )
+
+        gradients = convert_orientation_to_pole_vector(
+            azimuth=azimuth, 
+            dip=smaples, 
+            polarity=polarity
+        )
+
+        interp_input.orientations.dip_gradients = gradients
+        return interp_input
+
+    @staticmethod
+    def _update_model_for_plotting(geo_model: gp.data.GeoModel, sample_value: float, sample_idx: int):
+        # # Modify the surface point
+        gp.modify_orientations(
+            geo_model=geo_model,
+            dip=sample_value,
+        )
+    
+
+    def test_error_propagation(self, simple_geo_model):
+        """Test reading and computing a geological model with topography."""
+
+        geo_model = simple_geo_model
+        BackendTensor.change_backend_gempy(engine_backend=gp.data.AvailableBackends.PYTORCH)
+
+        mean_orientations = torch.ones(geo_model.orientations_copy.xyz.shape[0]) * 10
+        model_priors = {
+                r'dips': dist.Normal(
+                    loc=mean_orientations,
+                    scale=torch.tensor(10, dtype=torch.float64),
+                    validate_args=True
+                )
+        }
+
+        prob_model: gpp.GemPyPyroModel = gpp.make_gempy_pyro_model(
+            priors=model_priors,
+            set_interp_input_fn=self._modify_dips_for_orientations,
+            likelihood_fn=None,
+            obs_name=None
+        )
+
+        n_samples = 50
+        prior_inference_data: az.InferenceData = gpp.run_predictive(
+            prob_model=prob_model,
+            geo_model=geo_model,
+            y_obs_list=[],
+            n_samples=n_samples,
+            plot_trace=True
+        )
+
+        p2d = gpv.plot_2d(
+            model=geo_model,
+            show_topography=False,
+            legend=False,
+            show_lith=False,
+            show_data=False,
+            show=False
+        )
+
+        plot_gempy(
+            geo_model=geo_model,
+            n_samples=n_samples,
+            samples=(prior_inference_data.prior[r'dips'].values[0, :]),
+            update_model_fn=self._update_model_for_plotting,
+            gempy_plot=p2d
+        )
