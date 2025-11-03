81124646aaab39c4afb1d81de9834c666b8dd296
MaxHallenberger <max@terranigma-solutions.com>
2025-11-03 09:39:59 +0100

    commit of initialization files

---
+++ mineye/GeoModel/__init__.py
@@ -0,0 +1,10 @@
+"""GeoModel module for geological modeling workflows."""
+
+# Import helper modules
+from . import helper_methods as HelperMethods
+from . import helper_plotter as Plotter
+
+__all__ = [
+    'HelperMethods',
+    'Plotter',
+]
---
+++ mineye/config/__init__.py
@@ -0,0 +1,39 @@
+"""Configuration utilities."""
+from .paths import (
+    get_base_dir,
+    get_data_dir,
+    get_geomodel_dir,
+    get_topography_dir,
+    get_tmp_dir,
+    get_geophysical_dir,
+    get_model_paths,
+    get_model_extent,
+    get_model_resolution,
+    get_gis_map_info_path,
+    get_contact_points_path,
+    get_topography_path,
+    get_pickle_model_path,
+    get_orientations_path,
+    get_points_path,
+    get_gravity_data_path
+)
+
+__all__ = [
+    'get_base_dir',
+    'get_data_dir',
+    'get_geomodel_dir',
+    'get_topography_dir',
+    'get_tmp_dir',
+    'get_geophysical_dir',
+    'get_model_paths',
+    'get_model_extent',
+    'get_model_resolution',
+    'get_gis_map_info_path',
+    'get_contact_points_path',
+    'get_topography_path',
+    'get_pickle_model_path',
+    'get_orientations_path',
+    'get_points_path',
+    'get_gravity_data_path'
+]
+
