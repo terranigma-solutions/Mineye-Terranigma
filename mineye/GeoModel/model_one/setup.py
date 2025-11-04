4b9023b5e85089c7e7fbfc31f473e80a1d44ca42
Miguel de la Varga <miguel@terranigma-solutions.com>
2025-11-04 10:42:09 +0100

    [CLN] Move more code to a module
    # Conflicts:
    #	requirements.txt

--- mineye/GeoModel/model_one/setup.py
+++ mineye/GeoModel/model_one/setup.py
@@ -1,9 +1,12 @@
+import os
 from typing import Any
 
 import numpy as np
 
 import gempy as gp
 from gempy_engine.core.backend_tensor import BackendTensor
+
+import geopandas as gpd
 
 
 def baseline(geo_model) -> Any:
@@ -50,6 +53,34 @@ def setup_geomodel(gravity_data, simple_geo_model: gp.data.GeoModel):
     )
     gp.compute_model(geo_model)
     return geo_model, xy_ravel
+
+
+def read_gravity(geophysical_dir):
+    gravity_data = gpd.read_file(os.path.join(geophysical_dir, 'cleaned_gravity_data.geojson'))
+    observed_gravity = gravity_data['VALU_BOU267'].values  # in mGal
+
+    # Take a spatially distributed subset of measurements
+    n_points = 20  # Adjust this number to control how many points you want
+    xy_coords = gravity_data.geometry.apply(lambda p: (p.x, p.y)).to_list()
+    xy_array = np.array(xy_coords)
+
+    # Use K-means clustering to get well-distributed points
+    from sklearn.cluster import KMeans
+    kmeans = KMeans(n_clusters=n_points, random_state=42)
+    kmeans.fit(xy_array)
+
+    # Find the closest points to cluster centers
+    from scipy.spatial.distance import cdist
+    centers = kmeans.cluster_centers_
+    distances = cdist(centers, xy_array)
+    indices = [np.argmin(dist) for dist in distances]
+
+    # Filter the gravity data
+    observed_gravity = observed_gravity[indices]
+    gravity_data = gravity_data.iloc[indices]
+
+    observed_gravity_ugal = observed_gravity * 1000
+    return gravity_data, observed_gravity_ugal
 
 
 def _gravity_precomputations(density_plutonites: float, density_sedimentary_host: float,
