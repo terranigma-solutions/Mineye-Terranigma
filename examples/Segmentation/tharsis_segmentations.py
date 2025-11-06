d93d5b67f055da4f64b1a1a08171d4b1197f3806
simonvirgo <s.virgo@ged.rwth-aachen.de>
2025-11-06 13:58:43 +0100

    tidy up Segmentation Module files

--- examples/Segmentation/tharsis_segmentations.py
+++ examples/Segmentation/tharsis_segmentations.py
@@ -1,6 +1,5 @@
 # Import the run_workflow function from full_workflow
-from examples.Segmentation.full_workflow import run_workflow
-
+from mineye.Segmentation.full_workflow import run_workflow
 
 # %%  20m resolution
 bands = {
