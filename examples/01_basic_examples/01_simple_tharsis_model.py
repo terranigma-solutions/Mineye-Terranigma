f9feeb6f2b3fcdc95c3d1adc6272fd742cc7cd0a
MaxHallenberger <max@terranigma-solutions.com>
2026-01-26 10:58:13 +0100

    removed the plotter adjustments

--- examples/01_basic_examples/01_simple_tharsis_model.py
+++ examples/01_basic_examples/01_simple_tharsis_model.py
@@ -209,12 +209,6 @@ print("✓ Model with topography computed successfully")
 # of the geological model from any angle.
 
 import gempy_viewer as gpv
-import pyvista as pv
-
-# Configure PyVista to use smaller fonts for axes
-pv.global_theme.font.size = 10
-pv.global_theme.font.label_size = 10
-pv.global_theme.font.title_size = 12
 
 gpv.plot_3d(simple_geo_model, ve=5, image=True)
 
