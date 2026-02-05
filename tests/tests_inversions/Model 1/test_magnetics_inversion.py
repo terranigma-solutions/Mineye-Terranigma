9a6c084ca5bdaa0c31d965dee23977fd5638b042
Miguel de la Varga <miguel@terranigma-solutions.com>
2026-02-05 16:16:18 +0100

    [ENH] Update magnetics inversion tests for new dataset and model adjustments
    - Switched to the updated dataset `arviz_data_magnetic_Feb_05_I_hierarchical.nc` for testing.
    - Refined model configurations with changes to spatial resolution, grid radius, and Z elevation.
    - Simplified the Pyro model setup and removed unused arguments from the test methods.

--- tests/tests_inversions/Model 1/test_magnetics_inversion.py
+++ tests/tests_inversions/Model 1/test_magnetics_inversion.py
@@ -21,6 +21,10 @@ from mineye.GeoModel.plotting.probabilistic_analysis import plot_geophysics_comp
 # noinspection PyUnusedImport
 
 
+# nc_path = "arviz_data_magnetic_Nov_17_I_hierarchical.nc"
+nc_path = "arviz_data_magnetic_Feb_05_I_hierarchical.nc"
+
+
 class TestMagneticInversion:
     prior_key_dips = r'dips'
     prior_key_susceptibility = r'susceptibility'
@@ -45,7 +49,7 @@ class TestMagneticInversion:
 
     def test_magnetic_inversion(
             self, simple_geo_model, geophysical_dir, n_samples=50,
-            arviz_data_filename="arviz_data_magnetic_Nov_17_I_hierarchical.nc"
+            arviz_data_filename="arviz_data_magnetic_Feb_05_I_hierarchical.nc"
     ):
         """Test magnetic inversion using NUTS sampler."""
         print("Test magnetic inversion...")
@@ -78,8 +82,7 @@ class TestMagneticInversion:
                 max_tree_depth=5,
                 init_strategy='median',
                 num_samples=200,
-                warmup_steps=200,
-                num_chains=1
+                warmup_steps=200
             ),
             plot_trace=True,
             run_posterior_predictive=True
@@ -147,12 +150,10 @@ class TestMagneticInversion:
         )
 
         # * 6) Set up Pyro model
-        prob_model: gpp.GemPyPyroModel = gpp.make_gempy_pyro_model_extended(
+        prob_model: gpp.GemPyPyroModel = gpp.make_gempy_pyro_model(
             priors=model_priors,
             set_interp_input_fn=TestMagneticInversion._set_magnetic_priors,
             likelihood_fn=likelihood_fn,
-            pre_forward_deterministics={},
-            post_forward_deterministics=post_forward_dets,
             obs_name="Magnetic Measurement"
         )
         return geo_model, observed_magnetics_nt, prob_model
@@ -172,7 +173,7 @@ class TestMagneticInversion:
         xy_ravel = np.column_stack([
                 np.array(magnetic_data.geometry.x.values),
                 np.array(magnetic_data.geometry.y.values),
-                np.full(len(magnetic_data), 0)  # Set Z to surface elevation
+                np.full(len(magnetic_data), 500)  # Set Z to surface elevation
         ])
 
         # Setup centered grid for magnetics
@@ -180,7 +181,7 @@ class TestMagneticInversion:
             grid=simple_geo_model.grid,
             centers=xy_ravel,
             resolution=np.array([10, 10, 15]),
-            radius=np.array([5000, 5000, 5000])
+            radius=np.array([2000, 5000, 2000])
         )
 
         # Calculate magnetic gradient tensor
@@ -294,7 +295,7 @@ class TestMagneticInversion:
 
     def test_run_predictive_analysis(self, simple_geo_model, geophysical_dir):
         data = az.from_netcdf(os.path.join(
-            os.path.dirname(__file__), "arviz_data_magnetic_Nov_17_I_hierarchical.nc"
+            os.path.dirname(__file__), nc
         ))
         magnetic_data, observed_magnetics_nt = self._read_magnetics(geophysical_dir)
         geo_model, xy_ravel = self._setup_magnetic_geomodel(magnetic_data, simple_geo_model)
@@ -311,7 +312,7 @@ class TestMagneticInversion:
 
     def test_run_kde_sections(self, simple_geo_model, geophysical_dir):
         data = az.from_netcdf(os.path.join(
-            os.path.dirname(__file__), "arviz_data_magnetic_Nov_17_I_hierarchical.nc"
+            os.path.dirname(__file__), nc
         ))
 
         magnetic_data, observed_magnetics_nt = self._read_magnetics(geophysical_dir)
@@ -321,7 +322,7 @@ class TestMagneticInversion:
 
     def test_run_outlier_detection(self, simple_geo_model, geophysical_dir):
         data = az.from_netcdf(os.path.join(
-            os.path.dirname(__file__), "arviz_data_magnetic_Nov_17_I_hierarchical.nc"
+            os.path.dirname(__file__), nc
         ))
 
         posterior_sigmas = data.posterior_predictive["sigma_stations"].values  # shape: (chains, samples, 20)
@@ -350,7 +351,7 @@ class TestMagneticInversion:
 
     def test_run_analysis(self, simple_geo_model, geophysical_dir):
         data = az.from_netcdf(os.path.join(
-            os.path.dirname(__file__), "arviz_data_magnetic_Nov_17_I_hierarchical.nc"
+            os.path.dirname(__file__), nc
         ))
         data.posterior
 
